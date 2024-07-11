0d4a204d4a30f4e9cbe3c0c89fbf58e6f731899b
AffecT <artemhmaruk@gmail.com>
2024-07-12 01:00:11 +0300

    darkmode

--- frontend/src/features/handleDarkModeSwitch/handleDarkModeSwitch.tsx
+++ frontend/src/features/handleDarkModeSwitch/handleDarkModeSwitch.tsx
@@ -3,7 +3,9 @@ import { DarkModeSwitchUI } from "@/shared/darkModeSwitchUI";
 import MainStore from "@/store/store";
 
 export const handleDarkModeSwitch = () => {
-  document.documentElement.classList.add("dark");
+  Store.dark
+    ? document.documentElement.classList.remove("dark")
+    : document.documentElement.classList.add("dark");
   Store.dark ? Store.setDark(false) : Store.setDark(true);
 };
 
