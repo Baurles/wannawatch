0460a554914d76bdca0c6e246de63239e793b1f5
AffecT <artemhmaruk@gmail.com>
2024-07-12 00:01:30 +0300

    work

--- frontend/src/app/template.tsx
+++ frontend/src/app/template.tsx
@@ -13,28 +13,28 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <motion.div
         initial={{ opacity: 0 }}
         id="banner-text"
-        className="banner-text w-screen font-bold text-9xl justify-center items-center flex text-black h-screen fixed color-black z-30"
+        className="banner-text w-screen font-bold text-9xl justify-center items-center flex text-white h-screen fixed color-black z-30"
       >
         <h1>WANNA WATCH</h1>
       </motion.div>
       <div
         id="banner-1"
-        className="banner min-h-screen bg-white z-20 fixed top-0 left-0 w-1/4"
+        className="banner min-h-screen bg-black z-20 fixed top-0 left-0 w-1/4"
       ></div>
 
       <div
         id="banner-2"
-        className="banner min-h-screen bg-white z-20 fixed top-0 left-1/4 w-1/4"
+        className="banner min-h-screen bg-black z-20 fixed top-0 left-1/4 w-1/4"
       ></div>
 
       <div
         id="banner-3"
-        className="banner min-h-screen bg-white z-20 fixed top-0 left-2/4 w-1/4"
+        className="banner min-h-screen bg-black z-20 fixed top-0 left-2/4 w-1/4"
       ></div>
 
       <div
         id="banner-4"
-        className="banner min-h-screen bg-white z-20 fixed top-0 left-3/4 w-1/4"
+        className="banner min-h-screen bg-black z-20 fixed top-0 left-3/4 w-1/4"
       ></div>
       {children}
     </>
